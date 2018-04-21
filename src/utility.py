aa203ac57622e00a0a4b5503ac4fd3548f96d641
frankshyu <frank.la.sf@gmail.com>
2018-04-21 16:43:50 -0700

    updated input data

--- src/utility.py
+++ src/utility.py
@@ -1,8 +1,10 @@
 import sys
 import numpy as np
 import cv2
+DEBUG = True
 
 def readVideo(inputName):
+  print(f"reading video {inputName}")
   capture = cv2.VideoCapture(inputName)
   if (not capture.isOpened()):
     print("input video file invalid")
@@ -15,4 +17,8 @@ def readVideo(inputName):
     else:
       break
   capture.release()
+  if DEBUG:
+    cv2.imshow("first frame", ret[0])
+    cv2.imshow("last frame", ret[-1])
+    cv2.waitKey(10000)
   return ret
