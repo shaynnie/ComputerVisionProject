d4aa77fb38445c6399e0fb032bbbd0d3c9dae9b3
frankshyu <frank.la.sf@gmail.com>
2018-05-05 19:46:55 -0700

    naive stitching

--- src/stabilization.py
+++ src/stabilization.py
@@ -7,6 +7,7 @@ from scipy.ndimage.filters import gaussian_filter1d
 
 # Finds A: img2 = A * img1
 def getRigidTransform(img1, img2):
+  '''
 	# find the keypoints and descriptors with SIFT
 	sift = cv2.xfeatures2d.SIFT_create()
 	kp1, des1 = sift.detectAndCompute(img1,None)
@@ -25,24 +26,23 @@ def getRigidTransform(img1, img2):
 			good.append(m)
 	img1_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,2)
 	img2_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,2)
+  '''
+  orb = cv2.ORB_create()
+  kp1, des1 = orb.detectAndCompute(img1,None)
+  kp2, des2 = orb.detectAndCompute(img2,None)
 
-	# orb = cv2.ORB_create()
-	# kp1, des1 = orb.detectAndCompute(img1,None)
-	# kp2, des2 = orb.detectAndCompute(img2,None)
+  bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+  matches = bf.match(des1,des2)
+  img1_pts = np.float32([ kp1[m.queryIdx].pt for m in matches ]).reshape(-1,2)
+  img2_pts = np.float32([ kp2[m.trainIdx].pt for m in matches ]).reshape(-1,2)
 
-	# bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-	# matches = bf.match(des1,des2)
-
-	# img1_pts = np.float32([ kp1[m.queryIdx].pt for m in matches ]).reshape(-1,2)
-	# img2_pts = np.float32([ kp2[m.trainIdx].pt for m in matches ]).reshape(-1,2)
-
-	A = cv2.estimateRigidTransform(img1_pts, img2_pts, False)
-	if A is None:
-		return 0,0,0
-	dx = A[0,2]
-	dy = A[1,2]
-	da = np.arctan2(A[1,0], A[0,0])
-	return dx, dy, da
+  A = cv2.estimateRigidTransform(img1_pts, img2_pts, False)
+  if A is None:
+    return 0,0,0
+  dx = A[0,2]
+  dy = A[1,2]
+  da = np.arctan2(A[1,0], A[0,0])
+  return dx, dy, da
 
 def computeEuclieanMatrix(dx,dy,da):
 	A = np.zeros((2,3))
@@ -59,6 +59,20 @@ def getSubsetFrames(frames, p):
   for idx in p:
     newFrames.append(frames[idx])
   return newFrames
+
+def getAverage(outImg, j, k):
+  coorListX = [-1, 1, -1, 1]
+  coorListY = [1, -1, -1, 1]
+  rgb = np.array([0,0,0])
+  count = 0
+  for idx in range(len(coorListX)):
+    coorX = j + coorListX[idx]
+    coorY = k + coorListY[idx]
+    if coorX >= 0 and coorY >= 0 and coorX < outImg.shape[0] - 1 and coorY < outImg.shape[1] - 1:
+      rgb = rgb + outImg[coorX, coorY, :]
+      count += 1
+  rgb = rgb / count
+  return rgb
 
 def stablizedVideoRigid(allFrames, p):
   #-----------------------------------------------------#
@@ -98,8 +112,8 @@ def stablizedVideoRigid(allFrames, p):
   aTrajSmooth = gaussian_filter1d(aTraj, sigma=SIGMA)
 
   x = np.arange(len(xTraj))
-  plt.plot(x,xTraj,'r--', x,yTraj,'b--')
-  plt.plot(x,xTrajSmooth,'r:', x,yTrajSmooth,'b:')
+#  plt.plot(x,xTraj,'r--', x,yTraj,'b--')
+#  plt.plot(x,xTrajSmooth,'r:', x,yTrajSmooth,'b:')
   plt.show()
 
   corners = np.array([[0,0,1], [0,rows,1], [cols,0,1], [cols,rows,1]])
@@ -116,9 +130,33 @@ def stablizedVideoRigid(allFrames, p):
     da = transforms[i][2] + (aTrajSmooth[i] - aTraj[i])
     A = computeEuclieanMatrix(dx,dy,da)
     outImg = cv2.warpAffine(frames[i], A, (cols,rows))
-    mask = np.array(outImg != )
+    mask = np.invert(np.array(outImg != 0))
+    #######################################################
+    thisIdx = p[i]
+    searchRange = 20
+    for j in range(max(0, thisIdx - searchRange), min(len(allFrames) - 1, thisIdx + searchRange)):
+      if j == thisIdx:
+        continue
+      dx_1, dy_1, da_1 = getRigidTransform(allFrames[j], frames[i])
+      dx = dx_1 + transforms[i][0] + (xTrajSmooth[i] - xTraj[i])
+      dy = dy_1 + transforms[i][1] + (yTrajSmooth[i] - yTraj[i])
+      da = da_1 + transforms[i][2] + (aTrajSmooth[i] - aTraj[i])
+      A = computeEuclieanMatrix(dx,dy,da)
+      patch = cv2.warpAffine(allFrames[j], A, (cols, rows))
+      patch = np.multiply(patch, mask)
+      outImg = outImg + patch
+      mask = np.invert(np.array(outImg != 0))
+    #######################################################
+    '''
+    for j in range(outImg.shape[0]):
+      for k in range(outImg.shape[1]):
+        if not np.any(outImg[j,k,:]):
+          print(f"position {j}, {k} has no value")
+          outImg[j,k,:] = getAverage(outImg, j, k)
+    '''
+    #cv2.imwrite(f'cropped_frame{i}.jpg',outImg)
     outFrames.append(outImg)
-
+    '''
     outCorners = (A @ corners.T).T
     xl = max(outCorners[0,0], outCorners[1,0])
     xr = min(outCorners[2,0], outCorners[3,0])
@@ -130,6 +168,7 @@ def stablizedVideoRigid(allFrames, p):
     xRight = min(xRight, xr)
     yUp = max(yUp, yu)
     yDown = min(yDown, yd)
+    '''
   outFrames.append(frames[L-1])
   
   # cropping
